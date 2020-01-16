73fe6b829ca35a5455cd11a91d59d1426363299b
kaaare <33208333+kaaare@users.noreply.github.com>
2020-01-16 03:05:02 +0100

    Don't find nodes below origin, only bubble upwards
    Added artificial exclusion of results that are deeper than the origin node.
    
    Testing if the node found is a child of the node we bubble up from is necessary because querySelector is not supposed to stop searching and will find nodes below the starting point even if the while loop iterates up the tree.

--- parent-selector.js
+++ parent-selector.js
@@ -1,10 +1,17 @@
 HTMLElement.prototype.parentSelector = function(selector) {
+
+	let origo  = this;
 	let parent = this.parentNode;
+	
 	while (parent) {
 		let nodeFound = parent.querySelector(selector);
-		if (nodeFound) {
+		
+		// If node with provided selector is found, and it is not a child of the original node.
+		if (nodeFound && !origo.contains(nodeFound)) {
 			return nodeFound;
 		}
+		
 		parent = parent.parentNode;
 	}
+
 }
